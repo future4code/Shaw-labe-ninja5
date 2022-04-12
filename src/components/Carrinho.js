afc7cf46e81b574bbcb10a6a55f56eefa591154f
natanbreaks <natanlucas771@gmail.com>
2022-04-12 15:26:46 -0300

    Co-authored-by: matheus moraes <matesuboy@users.noreply.github.com> Co-authored-by: Wesley <wkdev7@users.noreply.github.com> Co-authored-by: mateusalda <mateusalda@users.noreply.github.com>

---
+++ src/components/Carrinho.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default class Carrinho extends React.Component {
+    render() {
+        return(
+            <h1>Carrinho</h1>
+        )
+    }
+}
---
+++ src/components/SerUmNinja.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default class SerUmNinja extends React.Component {
+    render(){
+        return(
+            <h1>SerUmNinja</h1>
+        )
+    }
+}
