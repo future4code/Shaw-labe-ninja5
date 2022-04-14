425c6d5656e2770979673360c0b888f9d0737e73
natanbreaks <natanlucas771@gmail.com>
2022-04-14 16:17:50 -0300

    Co-authored-by: Wesley <wkdev7@users.noreply.github.com>

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,53 +6,27 @@ import { Headers, BASE_URL } from "../const/Consts";
 
 
 export default class Cards extends React.Component {
-    state = {
-        cards: [],
-    }
-
-
-    componentDidMount = () => {
-        this.getAllJobs()
-    }
-
-
-    getAllJobs = () => {
-        const url = "https://labeninjas.herokuapp.com/jobs"
-        axios
-            .get(BASE_URL + "/jobs", Headers)
-            .then((res) => {
-                this.setState({ cards: res.data.jobs });
-            })
-            .catch((err) => {
-                alert(err.response.data.message)
-            })
-
-    }
-
-
 
     render() {
-
-
         let mapeamento
-        if (this.state.cards.length > 0) {
-            mapeamento = this.state.cards.map((card) => {
+        if (this.props.cards.length > 0) {
+            mapeamento = this.props.cards.map((card) => {
                 return (
                     <div key={card.id}>
                         <h3>{card.title}</h3>
                         <p>Até {card.dueDate} por {card.price}</p>
-                        <p onClick={this.props.screenSelect}>
+                        <p
+                            onClick={()=> this.props.screenSelect(card.id)}>
                             Ver detalhes
                         </p>
-                        <button onClick={() => this.props.addACarrinho(card.id)} >Add a Carrinho</button>
+                        <button onClick={this.props.addACarrinho} >Add a Carrinho</button>
                     </div>
-                )
+                );
+                console.log(card)
             })
         } else {
             mapeamento = <p> Carregando... </p>
         }
-
-
         return (
             <div>
                 {mapeamento}
--- src/components/ContratarUmNinja.js
+++ src/components/ContratarUmNinja.js
@@ -1,33 +1,57 @@
 import React from "react";
 import Cards from "./Cards";
 import DetailsCard from "./DetailsCard";
+import axios from "axios";
+import { Headers, BASE_URL } from "../const/Consts";
 
 
 
 export default class ContratarUmNinja extends React.Component {
     state = {
         SCRdetails: false, 
+        cards: [],
+        id: ""
+    }
+    
+
+
+    componentDidMount = () => {
+        this.getAllJobs()
     }
 
 
+    getAllJobs = () => {
+        const url = "https://labeninjas.herokuapp.com/jobs"
+        axios
+            .get(BASE_URL + "/jobs", Headers)
+            .then((res) => {
+                this.setState({ cards: res.data.jobs });
+            })
+            .catch((err) => {
+                alert(err.response.data.message)
+            })
 
-    screenSelect = () =>{
-        this.setState ({SCRdetails: !this.state.SCRdetails})
+    }
+    
+    screenSelect = (id) =>{
+        this.setState ({SCRdetails: !this.state.SCRdetails, id: id})
     }
     
     
+
     selectPage = () => {
         switch (this.state.SCRdetails){
             case true:
-                return <DetailsCard screenSelect={this.screenSelect}/>
-            case false:
-                return <Cards screenSelect={this.screenSelect}/>
-            default:
-                return <Cards screenSelect={this.screenSelect}/>
-        }
-    }
+                return <DetailsCard screenSelect={this.screenSelect} id={this.state.id}  />
+                case false:
+                    return <Cards screenSelect={this.screenSelect} cards={this.state.cards} />
+                    default:
+                        return <Cards screenSelect={this.screenSelect}/>
+                    }
+                }
+                
     render() {
-        
+
         return (
             <div>
                 <h1>ContratarUmNinja</h1>
