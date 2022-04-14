b6b024322f86db374124a8522b7e613c077c00bd
natanbreaks <natanlucas771@gmail.com>
2022-04-14 17:34:54 -0300

    resolucao de conflito

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,17 +8,16 @@ import { Headers, BASE_URL } from "../const/Consts";
 export default class Cards extends React.Component {
 
     render() {
-
         return (
             <div>
-                 <div key={this.props.card.id}>
-                        <h3>{this.props.card.title}</h3>
-                        <p>Até {this.props.card.dueDate} por {this.props.card.price}</p>
-                         <p onClick={()=> this.props.screenSelect(card.id)}>
-                            Ver detalhes
-                        </p>
-                        {/* <button onClick={() => this.props.addACarrinho(this.props.card.id)} >Add a Carrinho</button> */}
-                    </div>
+                <div key={this.props.card.id}>
+                    <h3>{this.props.card.title}</h3>
+                    <p>Até {this.props.card.dueDate} por {this.props.card.price}</p>
+                    <p onClick={() => this.props.goToDetailsJob(this.props.card.id)}>
+                        Ver detalhes
+                    </p>
+                    {/* <button onClick={() => this.props.addACarrinho(this.props.card.id)} >Add a Carrinho</button> */}
+                </div>
             </div>
         )
     }
